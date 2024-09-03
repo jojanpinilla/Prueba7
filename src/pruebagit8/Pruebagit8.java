caaa48a4cb40ae65ea5d84dee15ef35b16f60415
jojan25pr <jojan.chatgpt@gmail.com>
2024-09-03 16:04:22 -0500

    Segundo commit

--- src/pruebagit8/Pruebagit8.java
+++ src/pruebagit8/Pruebagit8.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pruebagit8;
 
-/**
- *
- * @author Angie
- */
 public class Pruebagit8 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Prueba 8");
+        
+        System.out.println("Prueba 8, cambio para el segundo commit");
     }
     
 }
