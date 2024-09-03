ef518ca59da75a081df375f65a024dee7affebb1
jojan25pr <jojan.chatgpt@gmail.com>
2024-09-03 16:08:38 -0500

    Commit para la rama1

--- src/pruebagit8/Pruebagit8.java
+++ src/pruebagit8/Pruebagit8.java
@@ -5,9 +5,10 @@ public class Pruebagit8 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Prueba 8");
         
         System.out.println("Prueba 8, cambio para el segundo commit");
+        
+        System.out.println("Rama 1, no master");
     }
     
 }
